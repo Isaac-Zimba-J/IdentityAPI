9ebd6987df64f6cc79898f5dd79456c23ae205e4
Zimba <74758329+Isaac-Zimba-J@users.noreply.github.com>
2024-06-25 15:28:16 +0200

    feat: Add ExpenseController with fetchExpenses endpoint
    The commit adds a new ExpenseController class with a fetchExpenses endpoint. This endpoint retrieves a list of expenses and returns them as a response.

---
+++ Controllers/ExpenseController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace IdentityAPI;
+
+[Route("api/[controller]")]
+[ApiController]
+
+public class ExpenseController : ControllerBase
+{
+    [HttpGet("get-expenses")]
+    public async Task<IActionResult> fetchExpenses()
+    {
+        var expenses = new List<Expense> {
+            new Expense {
+                expenseId = 1,
+                user = new CustomUser { Fname = "John", Lname = "Doe", Email = "isaac@gmail.com", PhoneNumber = 970187487, University = "CBU" },
+                amount = 23,
+                tag = new Tag { TagId = 1, TagName = "Internet", Colour= "#87987"},
+                date = new DateOnly(2021, 10, 10),
+                description = "Internet subscription",
+                expenseNumber = 1
+            }
+        };
+        return Ok(expenses);
+    }
+}
